4a73bbff4123c66f7db14ff6d5df228794a26d5c
Matías García Gimeno <mgg709@inlumine.ual.es>
2023-03-16 13:47:43 +0100

    metodos update y store review controller

--- videoclub/app/Http/Controllers/ReviewController.php
+++ videoclub/app/Http/Controllers/ReviewController.php
@@ -18,19 +18,30 @@ class ReviewController extends Controller
   }
 
   /**
-   * Show the form for creating a new resource.
+   * Show the form for creating a new resource. No tiene sentido este controlador, ya que la vista se hará con angular.
    */
-  public function create()
-  {
-    //
-  }
 
   /**
    * Store a newly created resource in storage.
    */
   public function store(Request $request)
   {
-    //
+    //Valido que recibo los campos
+    $request->validate([
+      'title' => 'required',
+      'description' => 'required',
+      'user_id' => 'required',
+      'movie_id' => 'required',
+    ]);
+    //Creo el objeto a partir de los datos recibidos, lo guardo y lo devuelvo.
+    $review = Review::create([
+      'title' => $request->string('title'),
+      'description' => $request->string('description'),
+      'user_id' => $request->int('user_id'),
+      'movie_id' => $request->int('movie_id'),
+    ]);
+    $review->save();
+    return $review->json_encode();
   }
 
   /**
@@ -44,19 +55,32 @@ class ReviewController extends Controller
   }
 
   /**
-   * Show the form for editing the specified resource.
+   * Show the form for editing the specified resource. No tiene sentido este controlador, ya que la vista se hará con angular.
    */
-  public function edit(string $id)
-  {
-    //
-  }
+  // public function edit(string $id)
+  // {
+  //   //
+  // }
 
   /**
    * Update the specified resource in storage.
    */
   public function update(Request $request, string $id)
   {
-    //
+    //Busco la review, valido los campos que recibo, actualizo los campos y guardo.
+    $review = Review::findOrFail($id);
+    $request->validate([
+      'title' => 'required',
+      'description' => 'required',
+      'user_id' => 'required',
+      'movie_id' => 'required',
+    ]);
+    $review->title = $request->string('title');
+    $review->description = $request->string('description');
+    $review->user_id = $request->int('user_id');
+    $review->movie_id = $request->int('movie_id');
+    $review->save();
+    return $review->json_encode();
   }
 
   /**
