ab40218846520f09568b1c329a918ac742c73cd8
Matías García Gimeno <mgg709@inlumine.ual.es>
2023-03-17 11:04:35 +0100

    tests update review completo

--- videoclub/tests/Feature/Http/Controllers/ReviewController/UpdateTest.php
+++ videoclub/tests/Feature/Http/Controllers/ReviewController/UpdateTest.php
@@ -14,24 +14,69 @@ class UpdateTest extends TestCase
   use RefreshDatabase;
   public function test_update_review(): void
   {
-    $user = User::factory()->create();
-    $movie = Movie::factory()->create();
     $review = Review::factory()->create();
     $review2 = [
       'title' => 'Test title',
       'description' => 'Test description',
-      'user_id' => $user->id,
-      'movie_id' => $movie->id,
+      'user_id' => $review->user_id,
+      'movie_id' => $review->movie_id,
     ];
     $response = $this->put(route('resenas.update', $review->id), $review2);
     $response->assertStatus(200);
     $response->assertJson([
       'title' => 'Test title',
       'description' => 'Test description',
-      'user_id' => strval($user->id),
-      'movie_id' => strval($movie->id),
+      'user_id' => strval($review->user_id),
+      'movie_id' => strval($review->movie_id),
     ]);
     $this->assertDatabaseHas('reviews', $review2);
     $this->assertDatabaseMissing('reviews', $review->toArray());
+  }
+
+  public function test_update_title_review()
+  {
+    $review = Review::factory()->create();
+    $review2 = [
+      'title' => 'Test title',
+      'description' => $review->description,
+      'user_id' => $review->user_id,
+      'movie_id' => $review->movie_id,
+    ];
+    $response = $this->put(route('resenas.update', $review->id), $review2);
+    $response->assertStatus(200);
+    $response->assertJson([
+      'title' => 'Test title',
+      'description' => strval($review->description),
+      'user_id' => strval($review->user_id),
+      'movie_id' => strval($review->movie_id),
+    ]);
+    $this->assertDatabaseHas('reviews', $review2);
+    $this->assertDatabaseMissing('reviews', $review->toArray());
+  }
+  public function test_update_review_non_existent()
+  {
+    $review = Review::factory()->create();
+    $review2 = [
+      'title' => 'Test title',
+      'description' => $review->description,
+      'user_id' => $review->user_id,
+      'movie_id' => $review->movie_id,
+    ];
+    $response = $this->put(route('resenas.update', 10), $review2);
+    $response->assertStatus(404);
+    $response->assertNotFound();
+  }
+
+  public function test_update_review_with_wrong_data()
+  {
+    $review = Review::factory()->create();
+    $response = $this->put(route('resenas.update', $review->id), [
+      'title' => '',
+      'description' => $review->description,
+      'user_id' => $review->user_id,
+      'movie_id' => $review->movie_id,
+    ]);
+    $response->assertStatus(302);
+    $response->assertSessionHasErrors('title');
   }
 }
