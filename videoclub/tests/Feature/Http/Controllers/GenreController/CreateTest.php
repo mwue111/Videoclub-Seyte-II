a330cffcf3f7056315c5343fb0bd5f7ffc4ddfb0
Matías García Gimeno <mgg709@inlumine.ual.es>
2023-03-21 12:19:16 +0100

    test funcion create genero

---
+++ videoclub/tests/Feature/Http/Controllers/GenreController/CreateTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\GenreController;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateTest extends TestCase
+{
+  use RefreshDatabase;
+  public function test_show_form(): void
+  {
+    $this->get(route("generos.create"))
+      ->assertStatus(200)
+      ->assertSee(route("generos.store"))
+      ->assertSee(route("generos.index"));
+  }
+
+  public function test_create_and_save_genre(): void
+  {
+    $data = [
+      "name" => "Ciencia Ficción"
+    ];
+
+    $this->post(route("generos.store"), $data)
+      ->assertRedirect(route("generos.index"));
+
+    $this->assertDatabaseHas("genres", $data);
+  }
+
+  public function test_name_is_required(): void
+  {
+    $data = [
+      "name" => ""
+    ];
+    $this->post(route("generos.store"), $data)
+      ->assertSessionHasErrors("name");
+  }
+
+  public function test_creat_existent_genre(): void
+  {
+    $data = [
+      "name" => "Moda"
+    ];
+    $this->post(route("generos.store"), $data)
+      ->assertRedirect(route("generos.index"));
+    $this->post(route("generos.store"), $data)
+      ->assertSessionHasErrors("name");
+  }
+}
