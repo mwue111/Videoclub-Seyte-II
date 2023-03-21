ebd619f7be62fccf3b9e77efe367c2fc2306ed84
Matías García Gimeno <mgg709@inlumine.ual.es>
2023-03-21 13:45:07 +0100

    test edit controlador genero realizado

---
+++ videoclub/tests/Feature/Http/Controllers/GenreController/EditTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\GenreController;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Genre;
+
+class EditTest extends TestCase
+{
+  use refreshDatabase;
+  public function test_show_old_genre_values(): void
+  {
+    $genre = Genre::factory()->create();
+    $this->get(route('generos.edit', $genre))
+      ->assertViewIs('genres.edit')
+      ->assertStatus(200)
+      ->assertSee($genre->name);
+  }
+  public function test_store_new_genre_values(): void
+  {
+    $genre = Genre::factory()->create();
+    $this->patch(route('generos.update', $genre), [
+      'name' => 'cambio',
+    ])
+      ->assertRedirect(route('generos.index'));
+    $this->assertDatabaseHas('genres', ['name' => 'cambio']);
+  }
+
+  public function test_not_store_invalid_values(): void
+  {
+    $genre = Genre::factory()->create();
+    $this->patch(route('generos.update', $genre), [
+      'name' => '',
+    ])
+      ->assertSessionHasErrors(['name']);
+    $this->assertDatabaseHas('genres', ['name' => $genre->name]);
+  }
+}
