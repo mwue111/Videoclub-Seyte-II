279ee10f7e719d4662f736743c192d5cc74f0260
Matías García Gimeno <mgg709@inlumine.ual.es>
2023-03-24 09:35:57 +0100

    show test user controller

---
+++ videoclub/tests/Feature/Http/Controllers/UserController/ShowTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\UserController;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class ShowTest extends TestCase
+{
+
+  public function test_show_user(): void
+  {
+    $user = User::factory()->create();
+    $response = $this->get(route('usuarios.show', $user->id));
+    $response->assertStatus(200);
+    $response->assertJson($user->toArray());
+  }
+  public function test_not_showing_user(): void
+  {
+    $response = $this->get(route('usuarios.show', 0));
+    $response->assertStatus(404);
+  }
+}
