a110bf4be3fb531b2976dc32c6d3e6bfd3d15aed
Matías García Gimeno <mgg709@inlumine.ual.es>
2023-03-27 12:01:38 +0200

    arreglo peticion destroy vista show genre

--- videoclub/resources/views/genres/show.blade.php
+++ videoclub/resources/views/genres/show.blade.php
@@ -1,3 +1,7 @@
 <p>{{$genre->name}}</p>
 <a href="{{ route('generos.index') }}">Volver</a>
-<a href=" {{route('generos.destroy', $genre->id) }}">Eliminar</a>
+<form action="{{route('generos.destroy', $genre->id) }}" method="POST">
+  @csrf
+  @method('DELETE')
+  <input type="submit" value="Eliminar">
+</form>
