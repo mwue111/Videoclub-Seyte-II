167ad924f55911948bacc795061affa9c58af5d4
Matías García Gimeno <mgg709@inlumine.ual.es>
2023-03-29 13:05:27 +0200

    creación de componente

--- videoclub-front/src/app/app.module.ts
+++ videoclub-front/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
