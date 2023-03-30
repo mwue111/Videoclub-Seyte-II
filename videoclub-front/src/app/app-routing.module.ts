e09244b231f76850974168864b2f776da390f231
Matías García Gimeno <mgg709@inlumine.ual.es>
2023-03-30 10:08:52 +0200

    navbar sin login completada

--- videoclub-front/src/app/app-routing.module.ts
+++ videoclub-front/src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(res => res.AuthModule) },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((res) => res.AuthModule),
+  },
 ];
 
 export const appRoutingProviders: any[] = [];
-export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(routes);
+export const routing: ModuleWithProviders<RouterModule> =
+  RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- videoclub-front/src/app/app.module.ts
+++ videoclub-front/src/app/app.module.ts
@@ -1,27 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule, appRoutingProviders, routing } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import {
+  AppRoutingModule,
+  appRoutingProviders,
+  routing,
+} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const material = [MatToolbarModule];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LandingComponent,
-    NavbarComponent,
-  ],
+  declarations: [AppComponent, LandingComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     routing,
     HttpClientModule,
     BrowserAnimationsModule,
+    material,
   ],
+  exports: [material, RouterModule],
   providers: [appRoutingProviders],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
