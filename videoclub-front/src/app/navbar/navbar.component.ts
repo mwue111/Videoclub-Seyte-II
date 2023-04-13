ae4d52dd7cc4cc341ca06b46227c083d61f9b386
Matías García Gimeno <mgg709@inlumine.ual.es>
2023-04-13 11:54:10 +0200

    cambios navbar

--- videoclub-front/src/app/navbar/navbar.component.ts
+++ videoclub-front/src/app/navbar/navbar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../modules/auth/_services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-
+  logged: boolean;
+  constructor(private _auth: AuthService) {
+    this.logged = _auth.isLogged();
+  }
 }
