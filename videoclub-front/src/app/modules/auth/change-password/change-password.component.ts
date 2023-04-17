86e0863798e4cfc645ca30c0b3d2523138afd03e
Matías García Gimeno <mgg709@inlumine.ual.es>
2023-04-17 11:48:39 +0200

    styles recover password

--- videoclub-front/src/app/modules/auth/change-password/change-password.component.ts
+++ videoclub-front/src/app/modules/auth/change-password/change-password.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../_services/auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { throwError } from 'rxjs';
@@ -9,7 +9,7 @@ import { throwError } from 'rxjs';
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.css']
 })
-export class ChangePasswordComponent {
+export class ChangePasswordComponent implements OnInit{
   changePasswordForm: FormGroup;
   errors: any;
   constructor(private fb: FormBuilder, private _auth: AuthService, private route: ActivatedRoute) {
@@ -22,6 +22,40 @@ export class ChangePasswordComponent {
     route.queryParams.subscribe(params => {
       this.changePasswordForm.controls['passwordToken'].setValue(params['token']);
     });
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+  
+initForm() {
+    this.changePasswordForm = this.fb.group({
+      email: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.email,
+          Validators.minLength(3),
+          Validators.maxLength(250)
+        ])
+      ],
+      password: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(100)
+        ])
+      ],
+      password_confirmation: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(100)
+        ])
+      ]
+    })
   }
 
   onSubmit() {
