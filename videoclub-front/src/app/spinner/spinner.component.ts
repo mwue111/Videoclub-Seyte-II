48a842547f17d55fdc97e6cc7a9812b17ecc0b5b
mwue111 <mwue111@g.educaand.es>
2023-04-18 13:13:57 +0200

    añadido comentario

--- videoclub-front/src/app/spinner/spinner.component.ts
+++ videoclub-front/src/app/spinner/spinner.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { LoaderService } from '../services/loader.service';
 
+//Aquí: https://danielk.tech/home/angular-how-to-add-a-loading-spinner
+
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
