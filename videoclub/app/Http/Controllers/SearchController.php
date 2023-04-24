ebbb55f3cdc9a4c2beb6a0407e4c89f47edaf625
mwue111 <mwue111@g.educaand.es>
2023-04-24 16:54:09 +0200

    borrado comentario

--- videoclub/app/Http/Controllers/SearchController.php
+++ videoclub/app/Http/Controllers/SearchController.php
@@ -8,34 +8,6 @@ use App\Models\Movie;
 class SearchController extends Controller
 {
     public function search(Request $request) {
-        // $data = $request->search;
-
-        // if(isset($data)) {
-        //     $querys = Movie::where('title', 'LIKE', '%'. $data . '%')
-        //                     ->orWhere('director', 'LIKE', '%' . $data . '%')
-        //                     ->orWhere('year', '=', $data)
-        //                     ->paginate(5);
-
-        //     if(!$querys->count()){
-        //         $querys = null;
-        //     }
-        // }
-        // else{
-        //     $querys = Movie::latest()->paginate(5);
-        // }
-
-        //con autocomplete de jQuery será necesario que se pase una key label:
-        // $response = [];
-
-        // foreach($querys as $query){
-        //     $response[] = [
-        //         // 'id' => $query->id,
-        //         'label' => $query->title
-        //     ];
-        // }
-
-        // return response()->json($response);
-        // return view('movies.found', ['movies' => $querys]);
 
         $querys = Movie::latest()->filter(request(['search']))->paginate(5);
         if(!$querys->count()){
