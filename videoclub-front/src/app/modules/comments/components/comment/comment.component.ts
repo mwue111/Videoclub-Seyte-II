4da93d364472ca129eadb97452453d25bc9b0199
mwue111 <mwue111@g.educaand.es>
2023-05-08 09:06:00 +0200

    se muestra el username adecuado en cada review

--- videoclub-front/src/app/modules/comments/components/comment/comment.component.ts
+++ videoclub-front/src/app/modules/comments/components/comment/comment.component.ts
@@ -9,16 +9,22 @@ import { CommentsService } from '../../_services/comments.service';
 })
 export class CommentComponent {
   @Input() comment!: CommentInterface;
-  user: any = '';
+  username: string = '';
 
   constructor(
     private _comments: CommentsService,
   ) {
+
     //Obtener los datos de cada autor de cada reseña:
-    // this._comments.getAuthors(21)
-    //     .subscribe((res: any) => {
-    //       console.log('reseñas: ', res);
-    //     })
+    this._comments.getAuthors(1)
+        .subscribe((res: any) => {
+          const user = res.find((user: any) => user.id === this.comment.user_id);
+          if(user){
+            this.username = user.username;
+          }
+        })
   }
+
+
 
 }
