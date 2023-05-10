70a5a4a3d95de0c484062fc52be06e2017bea753
mwue111 <mwue111@g.educaand.es>
2023-05-10 10:06:17 +0200

    cambio menor

--- videoclub-front/src/app/pipes/date-ago.pipe.ts
+++ videoclub-front/src/app/pipes/date-ago.pipe.ts
@@ -25,7 +25,7 @@ export class DateAgoPipe implements PipeTransform {
                 counter = Math.floor(seconds / intervals[i]);
                 if (counter > 0)
                     if (counter === 1) {
-                        return counter + ' ' + i; // singular (1 day ago)
+                        return 'hace ' + counter + ' ' + i; // singular (1 day ago)
                     } else {
                       // return counter + ' ' + i + 's ago'; // plural (2 days ago)
                       let ending = '';
@@ -42,7 +42,7 @@ export class DateAgoPipe implements PipeTransform {
                         case 'hora': ending = 's'; break;
                       }
 
-                      return counter + ' ' + i + ending;
+                      return 'hace ' + counter + ' ' + i + ending;
                     }
             }
         }
