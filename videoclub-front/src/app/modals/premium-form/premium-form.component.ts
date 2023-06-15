6b224892dbd657574e19f81a62b6edd5f24bc3f2
mwue111 <daniela.wuerich@gmail.com>
2023-06-15 09:28:47 +0200

    test sub 2

--- videoclub-front/src/app/modals/premium-form/premium-form.component.ts
+++ videoclub-front/src/app/modals/premium-form/premium-form.component.ts
@@ -26,10 +26,10 @@ export class PremiumFormComponent {
 
   ngOnInit(): void {
     //nuevo:
-
+    this.initConfig2();
 
     //antiguo:
-    this.initConfig();
+    // this.initConfig();
     this.addOneYear();
   }
 
@@ -42,6 +42,81 @@ export class PremiumFormComponent {
   //############################################################
   //Aquí: https://stackoverflow.com/questions/63201776/paypal-subscription-integration-and-revise-subscription-issues
   //github: https://github.com/prettydev/ngx-paypal-subscription/blob/master/src/app/plan-list/plan-list.component.ts
+
+  private initConfig2(): void {
+    this.payPalConfig = {
+      // fundingSource: [
+      //   // {{ fundingSources,list }}
+      // ],
+      style: {
+        layout: 'vertical',
+        shape: 'rect',
+        color: 'gold',
+        // color: (fundingSource==paypal.FUNDING.PAYLATER) ? 'gold' : '',
+      },
+      currency: "USD",
+      clientId: "<test>",
+
+      createOrderOnServer: async (data) => {
+        const response = await fetch("http://localhost:9597/orders/", {
+          method: "POST",
+        });
+        const details = await response.json();
+        return details.id;
+      },
+
+      createSubscription: (data: any, actions: any) => {
+        return actions.subscription.create({
+          plan_id: "P-3RX065706M3469222L5IFM4I",
+        });
+      },
+
+      authorizeOnServer: async (approveData, actions) => {
+        const response = await fetch(
+          `http://localhost:9597/orders/${approveData.orderID}/capture`,
+          {
+            method: "POST",
+          }
+        );
+        const details = await response.json();
+        // Three cases to handle:
+        //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+        //   (2) Other non-recoverable errors -> Show a failure message
+        //   (3) Successful transaction -> Show confirmation or thank you
+
+        // This example reads a v2/checkout/orders capture response, propagated from the server
+        // You could use a different API or structure for your 'orderData'
+
+        const errorDetail = Array.isArray(details.details) && details.details[0];
+        if (errorDetail && errorDetail.issue === 'INSTRUMENT_DECLINED') {
+          return actions.restart(); // Recoverable state, per:
+          // https://developer.paypal.com/docs/checkout/integration-features/funding-failure/
+        }
+
+        if (errorDetail) {
+          let msg = 'Sorry, your transaction could not be processed.';
+          if (errorDetail.description) msg += '' + errorDetail.description;
+          if (details.debug_id) msg += ' (' + details.debug_id + ')';
+          return alert(msg); // Show a failure message (try to avoid alerts in production environments)
+        }
+
+        // Successful capture! For demo purposes:
+        console.log('Capture result', details, JSON.stringify(details, null, 2));
+        const transaction = details.purchase_units[0].payments.captures[0];
+        alert('Transaction '+ transaction.status + ': ' + transaction.id + 'See console for all available details');
+      },
+
+      onCancel: (data, actions) => {
+        console.log("OnCancel", data, actions);
+      },
+      onError: (err) => {
+        console.log("OnError", err);
+      },
+      onClick: (data, actions) => {
+        console.log("onClick", data, actions);
+      },
+    };
+  }
 
   //############################################################
 
